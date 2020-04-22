566472911dd32a4251c5bec2440e30aaac3e9236
Stefan Nastasa <stefan21nastasa@gmail.com>
2020-04-22 16:01:11 +0300

    New try

--- P1/p1.py
+++ P1/p1.py
@@ -23,60 +23,26 @@ def sigmoid_derivative(x):
 class NeuralNetwork:
 
     def __init__(self):
-        self.weights1 = np.random.rand(784,16)
-        self.bias1 = np.random.rand(1,16)
-        self.weights2 = np.random.rand(16,16)
-        self.bias2 = np.random.rand(1,16)
-        self.weights3 = np.random.rand(16,10)
-        self.bias3 = np.random.rand(1,10)
-        self.deriv_cost = []
-        self.cost = {
-            "weights": [] ,
-            "biases": []  
-            }
+        self.w1 = np.random.rand(16,784)
+        self.b1 = np.random.rand(16,1)
+        self.w2 = np.random.rand(16,16)
+        self.b2 = np.random.rand(16,1)
+        self.w3 = np.random.rand(10,16)
+        self.b3 = np.random.rand(10,1)
 
-    def feedForward(self, inp, oup):
-        self.x = np.reshape(inp, (1,784))
-        self.y = np.zeros(10)
-        self.y[oup] = 1
-
-        self.z1 = np.dot(self.x,self.weights1)+self.bias1
-        self.layer1 = sigmoid(self.z1)
-        self.z2 = np.dot(self.layer1,self.weights2)+self.bias2
-        self.layer2 = sigmoid(self.z2)
-        self.z3 = np.dot(self.layer2,self.weights3)+self.bias3
-        self.output = sigmoid(self.z3)  
-        #print(self.output)
-#disclaimer - pos sa nu fie corect modul in care am inceput sa fac backprop
-    def backprop(self):
-        d_bias3 =   2*(self.y - self.output) * sigmoid_derivative(self.z3)
-        d_weights3 =    np.dot(self.layer2.T,d_bias3)
-        #print(np.shape(d_bias3))
-        #last layer
-        d_bias2 = np.dot(d_bias3,self.weights3.T) * sigmoid_derivative(self.z2)
-        d_weights2 = np.dot(self.layer1.T,d_bias2)
-        #print(np.shape(d_bias2))
-
-        d_bias1 = np.dot(d_bias2,self.weights2.T) * sigmoid_derivative(self.z1)
-        d_weights1 = np.dot(self.x.T,d_bias1)
+    def feedForward(self, x, y):
+        self.x = np.reshape(x, (784,1))
+        self.y = np.zeros((10,1), dtype=float)
+        self.y[y]=1
         
-        self.cost["weights"].append([d_weights1,d_weights2,d_weights3])
-        self.cost["biases"].append([d_bias1,d_bias2,d_bias3])
-
-    def modif(self):
-        w = [0,0,0]
-        b = [0,0,0]
-        for d_set in self.cost["weights"]:
-            for i in range(0,3):
-                w[i] += d_set[i]
-
-        for d_set in self.cost["biases"]:
-            for i in range(0,3):
-                b[i] += d_set[i]
-
-        for i in range(0,3):
-            w[i]/=len(self.cost["weights"])
-            b[i]/=len(self.cost["biases"])
+        self.z1 = np.dot(self.w1,self.x) + self.b1
+        self.a1 = sigmoid(self.z1)
+        self.z2 = np.dot(self.w2,self.a1) + self.b2
+        self.a2 = sigmoid(self.z2)
+        self.z3 = np.dot(self.w3,self.a2) + self.b3
+        self.yHat = sigmoid(self.z3)
+        
+        return self.yHat
 
 def convert(x):
     finArr = []
@@ -89,31 +55,9 @@ def convert(x):
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 first_instance = NeuralNetwork()
-
-print(convert(x_train[0]))
-for i in range(0,60000):
-    fp = convert(x_train[i])
-    a = np.asarray(fp)
-    first_instance.feedForward(a,y_train[i])
-    first_instance.backprop()
-    if i%50 == 0:
-        first_instance.modif()
-
-
-a = img_to_np("P1/pict2.jpg")
-a = np.array(a)
-first_instance.feedForward(a,3)
-print(first_instance.output)
-
-a = img_to_np("P1/pict1.jpg")
-a = np.array(a)
-first_instance.feedForward(a,2)
-print(first_instance.output)
-
-a = img_to_np("P1/pict3.jpg")
-a = np.array(a)
-first_instance.feedForward(a,1)
-print(first_instance.output)
+a = np.asarray(convert(x_train[0]))
+first_instance.feedForward(a, y_train[0])
+print(first_instance.y - first_instance.yHat)
 #print(x_train[0])
 
 
